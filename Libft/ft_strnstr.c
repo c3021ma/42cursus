8d88229899a9414d6f5c2c619e6197c287ab8cb6
c3021ma <c3021ma@gmail.com>
2022-11-07 20:19:43 +0100

    "updating ok paco"

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fmoreno- <fmoreno-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 10:53:41 by fmoreno-          #+#    #+#             */
-/*   Updated: 2022/11/07 19:41:27 by fmoreno-         ###   ########.fr       */
+/*   Updated: 2022/11/07 20:19:07 by fmoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*fthay;
-	char	*ftnee;
 
-	fthay = (char *)haystack;
-	ftnee = (char *)needle;
-	if (!*needle || !len)
+	if (!*needle)
 		return ((char *)haystack);
 	i = 0;
-	j = 0;
-	while (fthay[i] && i< len)
+	while (haystack[i] && i < len)
 	{
-		if (fthay[i] != ftnee[j])
-			i++;
-		else
-		{
-			j = 0;
-			while (fthay[i + j] == ftnee[j] && ftnee[j] && i + j < len)
-				j++;
-			if (!ftnee[j])
-				return ((char *)haystack + i);
-			i++;
-		}
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j] && i + j < len)
+			j++;
+		if (!needle[j])
+			return ((char *)haystack + i);
+		i++;
 	}
 	return (NULL);
 }
